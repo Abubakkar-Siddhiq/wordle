4fe340679439cf3cdc933b5f8858d14640f47498
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-11-20 11:48:29 +0530

    Update: updated title name and upadated App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import Keyboard from './components/Keyboard'
+import GameLayout from './Layout/GameLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    lol
-    </>
+    <main className="w-screen h-screen flex items-center justify-center">
+      <GameLayout />
+    </main>
   )
 }
 
