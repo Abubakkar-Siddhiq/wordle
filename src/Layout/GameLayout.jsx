ef7ae010a6373eaff20020b4946214b27dec413a
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-11-20 11:46:46 +0530

    Add: Layout for game screen

---
+++ src/Layout/GameLayout.jsx
@@ -0,0 +1,9 @@
+import Keyboard from "../components/Keyboard";
+
+export default function GameLayout() {
+    return (
+        <section className="w-[40rem] flex flex-col items-center justify-center">
+            <Keyboard />
+        </section>
+    )
+}
