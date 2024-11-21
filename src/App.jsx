e2a4559c058c71a8a9bada150623dd375824d5e1
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-11-21 21:38:30 +0530

    Update: GameLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import Keyboard from './components/Keyboard'
 import GameLayout from './Layout/GameLayout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className="w-screen h-screen flex items-center justify-center">
