f3f0708fd2e1292fefb749740f5cff035917c601
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-11-25 20:58:59 +0530

    Update: Renamed Cell to Line, Mapping from guess now in Board

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,24 +1,24 @@
-const Cell = ({ value }) => {
-    return (
-      <div className="flex gap-2">
-        {Array.from({ length: 5 }).map((_, i) => (
-          <h1 key={i} className="rounded-md border-2 border-gray-300 text-gray-800 text-3xl font-semibold w-16 h-16 grid place-items-center">
-            {value}
-          </h1>
-        ))}
-      </div>
-    );
+const Line = ({ value, solution }) => {
+  return (
+    <div className="flex gap-2">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <h1 key={i} className="rounded-md border-2 border-gray-300 text-gray-800 uppercase text-2xl font-semibold w-14 h-14 grid place-items-center">
+          {value[i]}
+        </h1>
+      ))}
+    </div>
+  );
 };
-  
-export default function Board() {
+
+export default function Board({ guess, solution }) {
   return (
     <div className="flex flex-col gap-2">
-        {
-            Array.from({ length: 5 }).map((_, i) => {
-                console.log(i)
-               return  <Cell value={i} />
-            })
-        }
+      {
+        guess.map((c, i) => {
+          console.log(c)
+          return <Line value={c ?? ''} />
+        })
+      }
     </div>
   )
 }
