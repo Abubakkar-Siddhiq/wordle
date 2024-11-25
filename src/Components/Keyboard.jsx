2421a5bb265c86890027bc907230b61231ad0a0e
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-11-25 21:03:02 +0530

    Update: Added bigKey prop to differentiate delete and Enter keys

--- src/Components/Keyboard.jsx
+++ src/Components/Keyboard.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-
-const Key = ({ value, isActive }) => <h1 className={`px-2 py-1 flex-1 rounded-md text-gray-700 font-bold h-12 grid place-items-center hover:bg-gray-200 ${ isActive ? 'bg-gray-100' : 'bg-gray-300' }`}>{value}</h1>; 
+const Key = ({ value, isActive, bigKey }) => <h1 className={`px-2 py-1 flex-1 rounded-md text-gray-700 font-bold h-12 grid place-items-center hover:bg-gray-200 ${bigKey && 'px-6'} ${ isActive ? 'bg-gray-100' : 'bg-gray-300' }`}>{value}</h1>; 
  
 export default function Keyboard( { activeKey, keyData } ) {
     
@@ -22,9 +20,11 @@ export default function Keyboard( { activeKey, keyData } ) {
             </div>
             <div className="w-full flex items-center gap-1">
                 {
-                    keyData.slice(19,).map(i => (
-                            <Key value={i} isActive={i == activeKey} />
-                        ))
+                    keyData.slice(19,).map(i => {
+                        return i.length > 1 
+                            ? <Key value={i} isActive={i == activeKey} bigKey /> 
+                            : <Key value={i} isActive={i == activeKey} />;
+                        })
                 }
             </div>
         </div>
