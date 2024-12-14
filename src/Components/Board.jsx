27fb196f80c836c417e943d5834f43d3a863eb66
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2024-12-14 21:08:10 +0530

    Update: Added lineIndex, currentGuess props

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,24 +1,18 @@
-const Line = ({ value, solution }) => {
+import Line from "./Line";
+
+export default function Board({ guess, solution, currentGuess }) {
   return (
-    <div className="flex gap-2">
-      {Array.from({ length: 5 }).map((_, i) => (
-        <h1 key={i} className="rounded-md border-2 border-gray-300 text-gray-800 uppercase text-2xl font-semibold w-14 h-14 grid place-items-center">
-          {value[i]}
-        </h1>
+    <div className="flex flex-col gap-1">
+      {guess.map((c, i) => (
+        <Line
+          key={i}
+          value={c ?? ''}
+          solution={solution}
+          currentGuess={currentGuess}
+          lineIndex={i}
+        />
       ))}
     </div>
   );
-};
-
-export default function Board({ guess, solution }) {
-  return (
-    <div className="flex flex-col gap-2">
-      {
-        guess.map((c, i) => {
-          console.log(c)
-          return <Line value={c ?? ''} />
-        })
-      }
-    </div>
-  )
 }
+
